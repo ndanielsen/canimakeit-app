1510a171e2a9caa07d067a88c9dad08a36944817
Max Heiber <max.heiber@gmail.com>
2015-07-29 22:10:25 -0400

    merge Nathan model and Mason states tmpl

---
+++ model/recommender.py
@@ -0,0 +1,64 @@
+#! usr/bin/env python -tt
+"""Recommendation Engine for Can I Make It 
+
+Input: User supplied information such salary, household size and etc
+
+Output: Returns nested dictionary of 3 budget types (min, mean, max) with a housing recommendation for each
+
+By Nathan Danielsen
+DDL4 Team
+"""
+import datetime
+
+from predict import Budgeter, Lodger, StateCluster
+
+class Base(object):
+	pass
+
+class Recommender(Base):
+	""" 
+	Inherits from Base-Object
+
+	Returns a nested dictionary of budgets each with housing recommendation
+	"""
+	def __init__(self):
+		
+		self.budgeting = Budgeter() # predict() returns max, mean, min budgets 
+		self.lodging = Lodger() # predict() returns three lodging options that housing prediction 
+		self.statecluster = StateCluster() # predict() returns list of states with clustering 1-5
+
+	def recommend(self, user_salary, rent=None, own=None, household_size=None ):
+		user_recommendations = {"timestamp": datetime.datetime.now().isoformat(' ')}		
+
+		budgets = self.budgeting.predict(user_salary)
+		
+		for b in budgets.keys():
+			# print b
+			housing = budgets[b]['housing']
+			budgets[b]['lodging'] = self.lodging.predict(housing) # returns dictionary of recommendations
+		
+		user_recommendations["budgeting"] = budgets	
+		user_recommendations['statecluster'] = self.statecluster.predict(user_salary)
+
+		return user_recommendations
+
+		
+
+if __name__ == '__main__':
+	import pprint
+	
+
+	main = Recommender()
+	pprint.pprint(main.recommend(40000))
+
+	lodge = Lodger()
+	assert lodge.predict(1000, preferences="this")[1]['type'] == '1BR'
+	print 'test lodger passed'
+
+	budget = Budgeter()
+	assert budget.predict(40000)['max']['housing'] == 6000.0
+	print 'test budget passed'
+
+	state = StateCluster()
+	assert state.predict(1000)[u'District of Columbia'] == 1
+	print 'test state passed'
--- server.py
+++ server.py
@@ -1,13 +1,27 @@
 from flask import Flask, request, render_template
-from model import info_to_making_it
+from model.recommender import Recommender
 import json
 
 app = Flask('canimakeit')
 
+states = ['AK', 'AL', 'AR', 'AS', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL',
+          'GA',
+          'GU', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD',
+          'ME',
+          'MI', 'MN', 'MO', 'MP',
+          'MS', 'MT', 'NA', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY',
+          'OH',
+          'OK', 'OR', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA',
+          'VI',
+          'VT', 'WA', 'WI', 'WV', 'WY']
+
+
+recommender = Recommender()
+
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', states=states)
 
 
 @app.route('/static/<path:path>')
@@ -18,9 +32,21 @@ def serve_static(path):
 @app.route('/api/user-info', methods=['POST'])
 def post():
     print '\nReceived: ', request.data, '\n\n'
-    data = request.data
-    making_it_by_state = info_to_making_it(data)
-    return json.dumps(making_it_by_state)
+    data = request.get_json()
+    salary = int(data['salary'])
+    # print 'salary is', salary
+    recommendation = recommender.recommend(user_salary=salary)
+    return json.dumps(recommendation['statecluster'])
+    # making_it_by_state = info_to_making_it(data)
+    # return json.dumps(making_it_by_state)
+
+
+# pass variables to html
+@app.route('/states/<state>')
+def state(state):
+    # return render_template('index.html')
+    return render_template('states.html', state=state, number=1)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
